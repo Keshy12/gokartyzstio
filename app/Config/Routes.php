2b5691a5baca6b320d19ab15d4c06a579ccf3849
Keshy12 <100865662+Keshy12@users.noreply.github.com>
2022-03-04 13:18:19 +0100

    Skracanie działa dla każdych wartości
    Skrócony link w routes przyjmuje wartości domyślne lub te wpisane w URL

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,7 +33,7 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 
 //$routes->get('/', 'Home::index');
-$routes->add('product','Shop::product');
+$routes->add('product/(:any)/(:any)','Shop::product/$1/$2');
 
 /*
  * --------------------------------------------------------------------

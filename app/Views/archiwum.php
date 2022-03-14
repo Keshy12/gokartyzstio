a8ad4782b7829fbe20a0cecd18edeb1365c5468d
Moparus <mzpotoniec@gmail.com>
2022-03-14 14:11:56 +0100

    Archiwum

--- app/Views/archiwum.php
+++ app/Views/archiwum.php
@@ -1,17 +1,62 @@
 <?= $this->extend('layouts/main')?>
 <?= $this->section('content')?>
-    <div class="card bg-secondary mb-3" style="max-width: 20rem;">
-        <div class="card-header">Header</div>
-        <div class="card-body">
-            <h4 class="card-title">Secondary card title</h4>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+<div class="container-fluid">
+    <h2 class="mt-3">Historia Zawodów</h2>
+    <div class="row mt-3">
+        <!-- PIERWSZY RZĄD -->
+        <div class="col">
+            <!-- ELEMENT NAJNOWSZY -->
+            <div class="card border-success bg-success mb-3" style="max-width: 50rem;">
+                <div class="card-body">
+                    <h4 class="card-title">Zawody Klasy 3-4</h4>
+                    <p >Data: 18.06.2022 / 18.07.2022 </p>
+                    <button type="button" class="btn btn-outline-dark">Tabela Wyników</button>
+                </div>
+            </div>
+            <!-- ELEMENT ZWYKLY -->
+            <div class="card border mb-3" style="max-width: 50rem;">
+                <div class="card-body">
+                    <h4 class="card-title">Zawody Klasy 3-4</h4>
+                    <p >Data: 18.06.2022 / 18.07.2022 </p>
+                    <button type="button" class="btn btn-outline-dark">Tabela Wyników</button>
+                </div>
+            </div>
+
+            <div class="card border mb-3" style="max-width: 50rem;">
+                <div class="card-body">
+                    <h4 class="card-title">Zawody Klasy 3-4</h4>
+                    <p >Data: 18.06.2022 / 18.07.2022 </p>
+                    <button type="button" class="btn btn-outline-dark">Tabela Wyników</button>
+                </div>
+            </div>
+        </div>
+        <!-- DRUGI RZĄD -->
+        <div class="col">
+            <div class="card border mb-3" style="max-width: 50rem;">
+                <div class="card-body">
+                    <h4 class="card-title">Zawody Klasy 1-2</h4>
+                    <p >Data: 18.06.2022 / 18.07.2022 </p>
+                    <button type="button" class="btn btn-outline-dark">Tabela Wyników</button>
+                </div>
+            </div>
+
+            <div class="card border mb-3" style="max-width: 50rem;">
+                <div class="card-body">
+                    <h4 class="card-title">Zawody Klasy 1-2</h4>
+                    <p >Data: 18.06.2022 / 18.07.2022 </p>
+                    <button type="button" class="btn btn-outline-dark">Tabela Wyników</button>
+                </div>
+            </div>
+
+            <div class="card border mb-3" style="max-width: 50rem;">
+                <div class="card-body">
+                    <h4 class="card-title">Zawody Klasy 1-2</h4>
+                    <p >Data: 18.06.2022 / 18.07.2022 </p>
+                    <button type="button" class="btn btn-outline-dark">Tabela Wyników</button>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="card text-white bg-success mb-3" style="max-width: 20rem;">
-        <div class="card-header">Header</div>
-        <div class="card-body">
-            <h4 class="card-title">Success card title</h4>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-    </div>
+</div>
 <?= $this->endSection() ?>

d93fcf8b0f6d3dcdde738abff59112181bf257fe
Keshy12 <100865662+Keshy12@users.noreply.github.com>
2022-03-14 17:15:33 +0100

    Logowanie, porownanie hasła z zhashowanym z bazy

--- app/Controllers/Gokarts.php
+++ app/Controllers/Gokarts.php
@@ -3,7 +3,7 @@
 namespace App\Controllers;
 
 // use App\Models\BlogModel;
-// use App\Models\CustomModel;
+use App\Models\CustomModel;
 class Gokarts extends BaseController
 {
     public function index()
@@ -31,7 +31,8 @@ class Gokarts extends BaseController
             'meta_title' => 'Tytuł strony',
         ];
         if(isset($_POST["userName"]) && isset($_POST["userPassword"])) {
-            if($_POST["userName"] == "user1" && $_POST["userPassword"] == "pass1") {
+            $pass = $model->getPass($_POST['userName']);
+            if(hash('sha256',$_POST['userPassword']) == $pass[0]->haslo) {
                 $_SESSION["zalogowany"] = "user1";
                 return view('gokarts',$data);
             } 

769cfcdf390341335fab8f04ca5655183cf687b5
Moparus <mzpotoniec@gmail.com>
2022-03-14 13:44:23 +0100

    Archiwum Update

--- app/Views/archiwum.php
+++ app/Views/archiwum.php
@@ -1,5 +1,17 @@
 <?= $this->extend('layouts/main')?>
 <?= $this->section('content')?>
-
-uwu
+    <div class="card bg-secondary mb-3" style="max-width: 20rem;">
+        <div class="card-header">Header</div>
+        <div class="card-body">
+            <h4 class="card-title">Secondary card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+    <div class="card text-white bg-success mb-3" style="max-width: 20rem;">
+        <div class="card-header">Header</div>
+        <div class="card-body">
+            <h4 class="card-title">Success card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
 <?= $this->endSection() ?>

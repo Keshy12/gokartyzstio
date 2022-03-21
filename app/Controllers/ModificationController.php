63c2fb1a62ad7a0042e4c5b8c7f0c32effd4b95b
Keshy12 <100865662+Keshy12@users.noreply.github.com>
2022-03-21 12:09:01 +0100

    Update ModificationController.php

--- app/Controllers/ModificationController.php
+++ app/Controllers/ModificationController.php
@@ -6,6 +6,38 @@ class ModificationController extends BaseController
 {
     function index()
     {
+        if($this->request->getMethod() == 'post'){
+            $rules = [
+                'email' => [
+                    'rules' => 'required|valid_email',
+                    'label' => 'Email adress',
+                    'errors' => [
+                        'required' => 'Email adress is a required field'
+                    ],
+                ],
+                'password' => 'required|min_length[8]',
+                'category' => 'in_list[Student, Teacher]',
+                'date' => [
+                    'rules' => 'required|check_date',
+                    'label' => 'Date',
+                    'errors' => [
+                        'check_date' => 'You need to specify a date before today'
+                    ]
+
+                ]
+                
+            ];
+    
+            if($this->validate($rules)){
+                //return redirect() -> to('/form/success');
+                //Then do database insertion
+                //Login user
+            }
+            else
+            {
+                $data['validation'] = $this->validator;
+            }
+        }
         $session = \Config\Services::session();
         if(!isset($_SESSION["zalogowany"]))
         {

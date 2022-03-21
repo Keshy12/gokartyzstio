1c097c36212d6e6e609eb901ac8e56de4e173479
Zeanyy <74882600+Zeanyy@users.noreply.github.com>
2022-03-21 12:28:13 +0100

    dziala mam nadzieje jak nie to magik

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -52,6 +52,8 @@ $routes->add('main/add/town', 'AppendController::addTown');
 $routes->add('main/add/gokart', 'AppendController::addGokart');
 $routes->add('main/compmod', 'CompModificationController');
 $routes->add('main/compmod/add', 'CompModificationController::addComp');
+$routes->add('main/compmod/begin', 'CompModificationController::beginComp');
+$routes->add('main/compmod/finish', 'CompModificationController::finishConp');
 $routes->add('main/judge', 'ArbiterController');
 $routes->add('main/judge/disqualify', 'ArbiterController::disqualify');
 $routes->add('main/judge/addTime', 'ArbiterController::addTime');
--- app/Models/CompModificationModel.php
+++ app/Models/CompModificationModel.php
@@ -8,5 +8,23 @@ class CompModificationModel extends AppendModel{
         $this->db =& $db;
     }
 
-    
+    public function changeState($id, $newState)
+    {
+        $this->db->query('UPDATE zawody SET status_zawodow_id = '.$newState.' WHERE zawody_id = '.$id);
+    }
+
+    public function getWithJoin($table, ...$joins)
+    {
+        $result = $this->db->table($table);
+        foreach($joins as $join)
+            $result->join($join[0], $join[1]);
+        return $result->get()->getResult();
+    }
+
+    public function remove($table, $id)
+    {
+        $result = $this->db->table($table)
+        ->where($table.'_id', $id)
+        ->delete();
+    }
 }

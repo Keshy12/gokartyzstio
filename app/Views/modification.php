afbda27c645280d1638d4a22d9d56acf16bac04c
Michaltbk <100926282+Michaltbk@users.noreply.github.com>
2022-03-21 12:17:58 +0100

    Revert "dodano detach"
    This reverts commit 766e9ed1ee91a2df84d8b41bbea1408ae531cb48.

--- app/Views/modification.php
+++ app/Views/modification.php
@@ -8,13 +8,13 @@
     $(document).ready(function(){
         var id0=$('#0'),id1=$('#1'),id2=$('#2'),id3=$('#3'),id4=$('#4'),id5=$('#5'),id6=$('#6'),id7=$('#7'),id8=$('#8'),id9=$('#9'),id10=$('#10');
         const id = [id0,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10];
-        $('.form').detach();
         id[<?php echo $_COOKIE["button"]?>].appendTo("#container");
         $("button").click(function(){
             $('.form').detach();
             $(id[$(this).val()]).appendTo("#container");
             Cookies.set('button', $(this).val())
         });
+
     });  
 </script>
 
